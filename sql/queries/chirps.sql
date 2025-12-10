eeff560fca3a3ef80a8d0d4986690291d9243b53
Bence Fekete <fekete965@gmail.com>
2025-12-10 20:04:33 +0000

    Add GetAllChirps query

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -2,3 +2,6 @@
 INSERT INTO chirps (id, user_id, body, created_at, updated_at)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
+
+-- name: GetAllChirps :many
+SELECT * FROM chirps ORDER BY created_at ASC;
