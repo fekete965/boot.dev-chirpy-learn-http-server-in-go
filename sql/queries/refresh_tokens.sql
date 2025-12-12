2170e8276a92a7aef6fe65ed65bea0bdc7a9d19f
Bence Fekete <fekete965@gmail.com>
2025-12-12 21:45:09 +0000

    Add refresh_token related queries

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,14 @@
+-- name: CreateRefreshToken :one
+INSERT INTO refresh_tokens (token, user_id, expires_at, revoked_at, created_at, updated_at)
+VALUES ($1, $2, $3, $4, $5, $6)
+returning *;
+
+-- name: FindRefreshToken :one
+SELECT rt.*
+FROM refresh_tokens rt
+INNER JOIN users u ON u.id = rt.user_id
+WHERE token = $1
+LIMIT 1;
+
+-- name: RevokeRefreshToken :exec
+UPDATE refresh_tokens SET revoked_at = $1, updated_at = $2 WHERE token = $3;
