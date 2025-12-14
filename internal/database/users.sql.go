bff2563fdd78c9b0fb16851219bbc7348eaa6ab4
Bence Fekete <fekete965@gmail.com>
2025-12-14 14:21:24 +0000

    Add UpdateUserIsChirpyRed query

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -117,6 +117,30 @@ func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, e
 	)
 	return i, err
 }
+
+const updateUserIsChirpyRed = `-- name: UpdateUserIsChirpyRed :one
+UPDATE users
+SET is_chirpy_red = $2, updated_at = $3
+WHERE id = $1
+RETURNING id, email, created_at, updated_at, hashed_password, is_chirpy_red
+`
+
+type UpdateUserIsChirpyRedParams struct {
+	ID          uuid.UUID
+	IsChirpyRed bool
+	UpdatedAt   time.Time
+}
+
+func (q *Queries) UpdateUserIsChirpyRed(ctx context.Context, arg UpdateUserIsChirpyRedParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, updateUserIsChirpyRed, arg.ID, arg.IsChirpyRed, arg.UpdatedAt)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Email,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.HashedPassword,
+		&i.IsChirpyRed,
 	)
 	return i, err
 }
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -17,3 +17,9 @@ UPDATE users
 SET email = $2, hashed_password = $3
 WHERE id = $1
 RETURNING *;
+
+-- name: UpdateUserIsChirpyRed :one
+UPDATE users
+SET is_chirpy_red = $2, updated_at = $3
+WHERE id = $1
+RETURNING *;
