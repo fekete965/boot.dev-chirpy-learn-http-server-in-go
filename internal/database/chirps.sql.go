b5e86e8b780c8efe1fccdb6fbdc4a5b455b4504c
Bence Fekete <fekete965@gmail.com>
2025-12-15 12:16:09 +0000

    Add GetAllChirpsByAuthorId query

--- internal/database/chirps.sql.go
+++ internal/database/chirps.sql.go
@@ -92,6 +92,39 @@ func (q *Queries) GetAllChirps(ctx context.Context) ([]Chirp, error) {
 	return items, nil
 }
 
+const getAllChirpsByAuthorId = `-- name: GetAllChirpsByAuthorId :many
+SELECT id, user_id, body, created_at, updated_at FROM chirps WHERE user_id = $1 ORDER BY created_at ASC
+`
+
+func (q *Queries) GetAllChirpsByAuthorId(ctx context.Context, userID uuid.UUID) ([]Chirp, error) {
+	rows, err := q.db.QueryContext(ctx, getAllChirpsByAuthorId, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Chirp
+	for rows.Next() {
+		var i Chirp
+		if err := rows.Scan(
+			&i.ID,
+			&i.UserID,
+			&i.Body,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const getChirpById = `-- name: GetChirpById :one
 SELECT id, user_id, body, created_at, updated_at FROM chirps WHERE id = $1 LIMIT 1
 `
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -6,9 +6,11 @@ RETURNING *;
 -- name: GetAllChirps :many
 SELECT * FROM chirps ORDER BY created_at ASC;
 
+-- name: GetAllChirpsByAuthorId :many
+SELECT * FROM chirps WHERE user_id = $1 ORDER BY created_at ASC;
+
 -- name: GetChirpById :one
 SELECT * FROM chirps WHERE id = $1 LIMIT 1;
-
 
 -- name: DeleteChirp :exec
 DELETE FROM chirps WHERE id = $1 AND user_id = $2;
