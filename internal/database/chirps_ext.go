826e13907a198fb9f7ad02b6ea8948bf9ecc5cac
Bence Fekete <fekete965@gmail.com>
2025-12-15 14:43:57 +0000

    Add custom database query in favor of generated one for retrieving chirps

---
+++ internal/database/chirps_ext.go
@@ -0,0 +1,79 @@
+package database
+
+import (
+	"context"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+type GetAllChirpsParams struct {
+	AuthorID *uuid.UUID
+	Sort *string
+}
+
+var DEFAULT_SORT string = "asc"
+func getSortQuery(sort *string, defaultSort *string) string {
+	sortValue := sort
+
+	if sortValue == nil {
+		sortValue = defaultSort
+	}
+
+	if sortValue == nil {
+		return ""
+	}
+
+	switch *sortValue {
+	case "desc":
+		return "ORDER BY created_at DESC;"
+	default:
+		return "ORDER BY created_at ASC;"
+	}
+}
+
+func (q *Queries) GetAllChirps(ctx context.Context, arg GetAllChirpsParams) ([]Chirp, error) {
+	var args []any
+
+	querySegments := []string{"SELECT * FROM chirps"}
+	if arg.AuthorID != nil {
+		querySegments = append(querySegments, "WHERE user_id = $1")
+		args = append(args, *arg.AuthorID)
+	}
+
+	sortQuery := getSortQuery(arg.Sort, &DEFAULT_SORT)
+	querySegments = append(querySegments, sortQuery)
+
+	rows, err := q.db.QueryContext(ctx, strings.Join(querySegments, " "), args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	
+	var items []Chirp
+
+	for rows.Next() {
+		var i Chirp
+		if err := rows.Scan(
+			&i.ID,
+			&i.UserID,
+			&i.Body,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		items = append(items, i)
+	}
+
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -3,12 +3,6 @@ INSERT INTO chirps (id, user_id, body, created_at, updated_at)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
--- name: GetAllChirps :many
-SELECT * FROM chirps ORDER BY created_at ASC;
-
--- name: GetAllChirpsByAuthorId :many
-SELECT * FROM chirps WHERE user_id = $1 ORDER BY created_at ASC;
-
 -- name: GetChirpById :one
 SELECT * FROM chirps WHERE id = $1 LIMIT 1;
 
