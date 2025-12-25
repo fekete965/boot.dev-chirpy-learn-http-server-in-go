6ea842beb665fdedadab1dbb3c2ec2954e2692ed
Bence Fekete <fekete965@gmail.com>
2025-12-25 22:55:18 +0100

    Add GetMigrationsDir test util

--- internal/testdb/utils.go
+++ internal/testdb/utils.go
@@ -3,11 +3,43 @@ package testdb
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/fekete965/boot.dev-chirpy-learn-http-server-in-go/internal/database"
 	"github.com/stretchr/testify/require"
 )
+
+func findProjectRoot() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("failed to find cwd: %v", err)
+	}
+
+	for {
+		goModPath := filepath.Join(dir, "go.mod")
+		if _, err := os.Stat(goModPath); err == nil {
+			return dir, nil
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			return "", os.ErrNotExist
+		}
+		dir = parent
+	}
+}
+
+func GetMigrationsDir(t *testing.T) string {
+	cwd, err := findProjectRoot()
+	if err != nil {
+		t.Fatalf("failed to find project root: %v", err)
+	}
+
+	return filepath.Join(cwd, "sql", "schema")
+}
 
 func WithTx(t *testing.T, ctx context.Context, db *sql.DB, fn func(*database.Queries) error) {
 	tx, err := db.BeginTx(ctx, nil)
